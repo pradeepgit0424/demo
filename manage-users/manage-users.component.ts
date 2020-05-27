a07b63b1a4e2962a0feaa7aabb1e4db1cac2acd5
pradeepgit0424 <55503474+pradeepgit0424@users.noreply.github.com>
2020-05-27 18:50:16 +0530

    27/05/2020 Manage User component

--- manage-users/manage-users.component.ts
+++ manage-users/manage-users.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
-
+import { UserType, UserTypeLst } from '../../models';
 
 @Component({
   selector: 'app-manage-users',
@@ -10,26 +9,34 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./manage-users.component.css']
 })
 export class ManageUsersComponent implements OnInit {
-  adminDetails: LocalDataSource; 
+  adminDetails: LocalDataSource;
+  CommomUserDetails: LocalDataSource;
   selectedUserTypeId: number;
   selectedDisplayName: string;
-  selectedUserEmailId: string; 
+  selectedUserEmailId: string;
   selectedUserName: string;
   selectedUserPassword: string;
+  userDetail = new UserType;
   userTypeId = 0; // variable to hold userType ID
   createdDate: Date;
-  userId = 0; 
+  loggedInUserId: any; // login id of the current user
+  userTypeList: typeof UserTypeLst = UserTypeLst; // constant variable that fill up type of users into dropdown
+  userId = 0;
   isSubmitted = false;
-  userTypeList : any;
-   // form group declarations
-   userForm: FormGroup;
-   firstName: FormControl;
-   lastName: FormControl;
-   email: FormControl;
-   password: FormControl;
-   language: FormControl;
+  naCheckbox = false;
 
-   tableSettings = {
+  userForm: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
+  SelectedUserDetail: any;
+  userData: any[];
+  isEdit: boolean;
+
+
+  tableSettings = {
     columns: {
       //Column 1
       userName: {
@@ -37,10 +44,6 @@ export class ManageUsersComponent implements OnInit {
         title: 'User Name',
         width: '10%'
       },
-      userType: {
-        title: 'User Name ',
-        width: '10%'
-      }
     },
     //For Search Textbox in each column
     hideSubHeader: true,
@@ -72,19 +75,264 @@ export class ManageUsersComponent implements OnInit {
     }
   };
 
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+    this.getAllUsers();
+    //this.userDetails = new UserDetails();
   }
-// method for when a dropdown value is changed
-onChangeDropDown() {
-  this.isSubmitted = false;
-}
-clearAllVariables(){
+  // method for when a dropdown value is changed
+  onChangeDropDown() {
+    this.isSubmitted = false;
+  }
 
-}
-saveClick(){
+  // method to get all users
+  getAllUsers() {
+    this.userData = USERLST;
 
+    this.adminDetails = new LocalDataSource(this.userData);
+
+    // this._userService.getAllUsers().subscribe(res => {
+    //   this.allUsersDetails = res;
+    //   this.adminDetails = new LocalDataSource(res.filter(users => users.userTypeId === 1));
+    //   this.CommomUserDetails = new LocalDataSource(res.filter(users => users.userTypeId === 2));
+    // });
+  }
+
+  // Function to edit a Brand
+  editUser(UserTypeDetail: any) {
+    this.isEdit = true;
+    this.setUserDetails(UserTypeDetail.data);
+    this.SelectedUserDetail = UserTypeDetail;
+    console.log(this.SelectedUserDetail);
+  }
+
+  setUserDetails(userDetail: UserType) {
+    setTimeout(() => {
+      this.userForm.patchValue({
+        firstName: userDetail.displayName,
+        email: userDetail.userEmailId,
+        lastName: userDetail.userName,
+        password: userDetail.userPassword
+      });
+      this.selectedUserTypeId = userDetail.userTypeId;
+      this.userId = userDetail.userId;
+      this.naCheckbox = userDetail.isSuggestionRecipient;
+    }, 100);
+  }
+
+  // fill the data into object to save
+  fillUserDetailsToObject() {
+    this.userDetail.userName = this.userForm.value.lastName;
+    this.userDetail.displayName = this.userForm.value.firstName;
+    this.userDetail.userEmailId = this.userForm.value.email;
+    this.userDetail.userPassword = this.userForm.value.password;
+    this.userDetail.userTypeId = this.selectedUserTypeId;
+    this.userDetail.userId = this.userId;
+    this.userDetail.isSuggestionRecipient = this.naCheckbox;
+    if (this.userDetail.userId && this.userDetail.userId > 0) {
+      this.userDetail.updatedBy = this.loggedInUserId;
+    } else {
+      this.userDetail.createdBy = this.loggedInUserId;
+    }
+  }
+
+
+  clearAllVariables() {
+    this.selectedUserTypeId = null;
+    this.userForm.value.firstName = null;
+    this.userForm.value.email = null;
+    this.userForm.value.lastName = null;
+    this.userForm.value.password = null;
+    this.userId = 0;
+    this.naCheckbox = false;
+    this.userForm.reset();
+  }
+  // creates form control
+  createFormControls() {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,3}$')
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+      Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}')
+    ]);
+    this.language = new FormControl('');
+  }
+
+  // new form is created
+  createForm() {
+    this.userForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password,
+      language: this.language
+    });
+  }
+
+
+  saveClick() {
+
+    this.fillUserDetailsToObject();
+    this.saveUserDetails();
+    this.userData[0].displayName = this.selectedDisplayName;
+    this.userData[0].userEmailId = this.selectedUserEmailId;
+    this.userData[0].userType = this.userTypeId;
+    this.userData[0].userName = this.selectedUserName;
+    this.userData[0].userPassword = this.selectedUserPassword;
+    this.adminDetails = new LocalDataSource(this.userData);
+    console.log(this.userData[0]);
+  }
+
+  saveUser() {
+    // fill the data into object to save
+    this.fillUserDetailsToObject();
+    this.isEdit = false;
+    this.saveUserDetails();
+    console.log();
+    // metod to save brand details
+    //this.saveUserDetails();
+
+  }
+  saveUserDetails() {
+    // this._userService.createUser(this.userDetail).subscribe(res => {
+    //   if (res) {
+    //     if (this.userDetail.userId > 0) {
+    //       this.alertSerive.create('User Updated Successfully', 'success', 2000, '', []);
+    //     } else {
+    //       this.alertSerive.create('User Created Successfully', 'success', 2000, '', []);
+    //     }
+    //     this.userDetail = new UserType();
+    //     this.clearAllVariables();
+    //     this.getAllUsers();
+    //   } else {
+    //     if (this.userDetail.userId > 0) {
+    //       this.alertSerive.create(
+    //         'User Updation failed', 'danger', 2000, '', []
+    //       );
+    //     } else {
+    //       this.alertSerive.create(
+    //         'User Creation failed', 'danger', 2000, '', []
+    //       );
+    //     }
+
+    //   }
+    // });
+
+   }
+  // submit method
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.saveUser();
+      this.userForm.reset();
+    } else {
+      alert('error');
+    }
+  }
+
+  // Function to delete a Parameter
+  deleteUserPopup(UserTypeDetail: any) {
+    // this.userIdToDelete = UserTypeDetail.data.userId;
+    // this.alertSerive.create(
+    //   'Delete User', // title
+    //   'danger', // type
+    //   0, // time
+    //   'Are you sure want to delete?', // body
+    //   [
+    //     {
+    //       text: 'Yes',
+    //       buttonClass: 'btn btn-sm btn-jrrdan',
+    //       onAction: () => new Promise((resolve: any) => {
+    //         this.deleteUser();
+    //       })
+
+    //     },
+    //     {
+    //       text: 'No',
+    //       buttonClass: 'btn btn-sm btn-clear'
+    //     }
+    //   ]
+    // );
+  }
+
+  // method to delete user
+  deleteUser() {
+    // this._userService.deleteUser(this.userIdToDelete, true).subscribe(res => {
+    //   this.clearAllVariables();
+    //   this.getAllUsers();
+    //   this.alertSerive.create(
+    //     'User Deleted Successfully', // title
+    //     'success', // type
+    //     2000, // time
+    //     '', // body
+    //     []
+    //   );
+    // });
+  }
+
+  // Push a search term into table to search
+  searchUser(term: string, type: string): void {
+    switch (type) {
+      case 'admin':
+        if (term) {
+          this.adminDetails.setFilter([
+            // fields we want to include in the search
+            {
+              field: 'userName',
+              search: term
+            },
+          ], false);
+        } else {
+          this.adminDetails.setFilter([], true);
+        }
+        break;
+      case 'commonuser':
+        if (term) {
+          this.CommomUserDetails.setFilter([
+            // fields we want to include in the search
+            {
+              field: 'userName',
+              search: term
+            },
+          ], false);
+        } else {
+          this.CommomUserDetails.setFilter([], true);
+        }
+        break;
+    }
+  }
 }
 
-}
+
+
+
+const USERLST = [
+  {
+    userId: 1,
+    userName: 'John',
+    displayName: 'Sean',
+    //userType: 'Admin',
+    userTypeId: 1,
+    userEmailId: 'john@g.com',
+    userPassword: 'W5gtrty'
+  }
+]
+
+
+// class UserDetails {
+//     userId: any;
+//     userName: any;
+//     displayName:any
+//     //userType: 'Admin',
+//     userTypeId: any;
+//     userEmailId:any;
+//     userPassword: any;
+// }
